95156b084c6e084895b0133d7fc5d520dc1b9415
soul-chaser <122245131+soul-chaser@users.noreply.github.com>
2023-01-26 10:44:12 +0000

    Add name property to the WasRun class and change run function get function by name property and call for call function dynamically

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ class WasRun:
 
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
 
     def testMethod(self):
         self.wasRun = 1
 
     def run(self):
-        self.testMethod()
+        method = getattr(self, self.name)
+        method()
 
 
 test = WasRun("testMethod")
