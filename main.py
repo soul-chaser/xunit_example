1ae93bbb745369ab71fda00c30dd531e704a12e2
soul-chaser <122245131+soul-chaser@users.noreply.github.com>
2023-01-26 10:32:40 +0000

    Define empty testMethod function in the WasRun class to fix runtime exception

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ class WasRun:
     def __init__(self, name):
         self.wasRun = None
 
+    def testMethod(self):
+        pass
+
 
 test = WasRun("testMethod")
-print test.wasRun
+print(test.wasRun)
 test.testMethod()
-print test.wasRun
+print(test.wasRun)
