efe113a4a41ae225bc3db55b95d0f783dea7212e
soul-chaser <122245131+soul-chaser@users.noreply.github.com>
2023-01-26 10:55:14 +0000

    Change test codes automatically running using new TestCaseTest class testRunning function

--- main.py
+++ main.py
@@ -18,7 +18,13 @@ class WasRun(TestCase):
         self.wasRun = 1
 
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert (not test.wasRun)
+        test.run()
+        assert (test.wasRun)
+
+
+TestCaseTest("testRunning").run()
