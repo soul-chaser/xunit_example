78a11ba2652a625d2b3698ad011dec64ce47ecd3
sjin-hahn <sjin.hahn@samsung.com>
2023-01-30 16:08:24 +0900

    Add testFailedResultFormatting test method to the TestCaseTest class to check TestResult class's summary method correctly returns failed count or not

--- main.py
+++ main.py
@@ -54,6 +54,12 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert("1 run, 1 failed" == result.summary())
 
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
+
 
 class TestResult:
 
