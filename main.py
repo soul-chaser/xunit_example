1f425b4eca776332ff99317879eddf1940f4e77b
sjin-hahn <sjin.hahn@samsung.com>
2023-01-30 15:12:26 +0900

    Add runCount property holding expected run count value and change summary method returns result using runCount value

--- main.py
+++ main.py
@@ -46,8 +46,12 @@ class TestCaseTest(TestCase):
 
 
 class TestResult:
+
+    def __init__(self):
+        self.runCount = 1
+
     def summary(self):
-        return "1 run, 0 failed"
+        return "%d run, 0 failed" % self.runCount
 
 
 TestCaseTest("testTemplateMethod").run()
