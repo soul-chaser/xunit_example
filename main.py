9d70bf8282cc2cc0a487f442c2819f250a29814b
sjin-hahn <sjin.hahn@samsung.com>
2023-01-30 14:57:33 +0900

    Add tearDown string after previous assert compare string to check tearDown method called after testMethod call and delete useless wasRun property from WasRun class and add tearDown method call after method method call from TestCase class run method and add tearDown method to the WasRun class with logging method name string and add definition of the empty tearDown method

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ class TestCase:
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
+
+    def tearDown(self):
+        pass
 
 
 class WasRun(TestCase):
@@ -18,12 +22,13 @@ class WasRun(TestCase):
         TestCase.__init__(self, name)
 
     def testMethod(self):
-        self.wasRun = 1
         self.log = self.log + "testMethod "
 
     def setUp(self):
-        self.wasRun = None
         self.log = "setUp "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 
 class TestCaseTest(TestCase):
@@ -31,7 +36,7 @@ class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert ("setUp testMethod " == test.log)
+        assert ("setUp testMethod tearDown " == test.log)
 
 
 TestCaseTest("testTemplateMethod").run()
