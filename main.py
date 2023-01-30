cc127adfa459baa07f60230b0e8296fbb46dc4d6
sjin-hahn <sjin.hahn@samsung.com>
2023-01-30 14:39:32 +0900

    Add log property to the WasRun class recording method call sequence and replace assert comparing log property value instead of check wasSetUp flag property

--- main.py
+++ main.py
@@ -19,10 +19,11 @@ class WasRun(TestCase):
 
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + "testMethod "
 
     def setUp(self):
         self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
 
 
 class TestCaseTest(TestCase):
@@ -36,7 +37,7 @@ class TestCaseTest(TestCase):
 
     def testSetUp(self):
         self.test.run()
-        assert (self.test.wasSetUp)
+        assert ("setUp testMethod " == self.test.log)
 
 
 TestCaseTest("testRunning").run()
